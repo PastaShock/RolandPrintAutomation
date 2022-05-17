b96c0461c66c88210d0ef0a2cf11f840fa952a76
george pastushok <gpastushok@yahoo.com>
2022-05-17 14:16:09 -0700

    updated regex to be even more thorough after coming across additional variations on filenames

--- unhash.ps1
+++ unhash.ps1
@@ -1,22 +1,32 @@
-# $a = get-childitem -include '*-*-*-*.eps' -r
+# script to remove UUID, prepended text and appended text to logo file names.
+# george pastushok 2019-2022
 
-# for ($i = 0; $i -lt $a.count; $i++) {
-#     $newName = $a[$i].name.substring(0, $a[$i].BaseName.length-37) + '.eps'
-#     if (Test-Path $newName) {
-#         Write-Output "`t deleting file $a[$i].name"
-#         Remove-Item $a[$i].name
-#     } else {
-#         Write-Output "`t renaming file: $newName"
-#         $a[$i] | rename-item -newname {$newName}
-#     }
-# }
+function unUUID($filename) {
+    # find the files that match the UUID pattern
+    $uuidPattern = '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}.eps';
+    $a = get-childitem -include "*.eps" -r
+
+    for ($i = 0; $i -lt $a.count; $i++) {
+        $newName = $a[$i].name.substring(0, $a[$i].BaseName.length - 37) + '.eps'
+        if (Test-Path $newName) {
+            Write-Output "`t deleting file $a[$i].name"
+            Remove-Item $a[$i].name
+        }
+        else {
+            Write-Output "`t renaming file: $newName"
+            $a[$i] | rename-item -newname { $newName }
+        }
+    }
+}
 
 # define function to take a filename and rename it/trim off excess text
 function renameRegex($filename) {
-    return $filename.name -replace "^(?:[A-z_0-9-]+)(\d{6}_[ds]+)( \(\d{1,2}\))?", '$1'
+    # ^(order_design_\d{5}-)*(\d{6}_[dsx0-9]+)([A-z0-9]+)?(-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})?([ ()0-9]+)?(.eps)
+    return $filename.name -replace "^(order_design_\d{5}-)*(\d{6}_[dsx0-9]+)([A-z0-9]+)?(-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})?([ ()0-9]+)?(.eps)", '$2$6'
+    # return $filename.name -replace "^(?:[A-z_0-9-]+)(\d{6}_[ds]+)( \(\d{1,2}\))?", '$1'
 }
 
-$c = Get-ChildItem | Where-Object {$_.Name -match "\(*\).eps"}
+$c = Get-ChildItem | Where-Object { $_.Name -match "\(*\).eps" }
 for ($i = 0; $i -lt $c.count; $i++) {
     $oldName = $c[$i] -split " "
     $newName = $oldName[0] + ".eps"
@@ -26,21 +36,25 @@ for ($i = 0; $i -lt $c.count; $i++) {
 
 # the matching regex should look for 
 # ^[order_design_\d{5}-]+(\d{5}_[ds]+)( \(\d{1,2}\))?.eps
-$b = Get-ChildItem | Where-Object {$_.Name -match "order_design_\d{4,5}-*"}
+# or:
+# ^(order_design_\d{5}-)*(\d{6}_[dsx0-9]+)([A-z0-9]+)?(-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})?([ ()0-9]+)?(.eps)
+$b = Get-ChildItem | Where-Object { $_.Name -match "order_design_\d{4,5}-*" }
 for ($i = 0; $i -lt $b.count; $i++) {
     # $oldName = ($b[$i] -split "-")
-    for ($j = 1; $j -lt $oldName.count; $j++) {
+    for ($j = 1; $j -lt $b.count; $j++) {
         $newName = renameRegex($b[$i]);
+        write-output "newName = $newName";
         # $newName += $oldName[$j];
     }
     #pattern = order_design_{4-6 digits}-{fundId}_{logoSize}_{logoScript}
-    if ($newName -eq "") {return}
+    if ($newName -eq "") { return }
     if (Test-Path $newName) {
         Write-Output "`t deleting file $newName`n"
         Remove-Item $newName
-    } else {
+    }
+    else {
         Write-Output "`t renaming file: $newName`n"
-        $b[$i] | rename-item -newname {$newName}
+        $b[$i] | rename-item -newname { $newName }
     }
 }
 Remove-Item "*(*).pdf"
