a3803275ea7803c27c6762446f9ffe2d2ff42064
pastashock <gpastushok@yahoo.com>
2023-03-03 16:11:50 -0800

    removed redundancies and greatly improved execution speed

--- csvTool.ps1
+++ csvTool.ps1
@@ -1,45 +1,42 @@
 # Fill empty date cells with todays date
+# Clean up rows with edits in them and parenthesis
+
+$csv = Import-CSV .\reprints.csv;
+
 Function csvfill {
 	param (
 		$subCatDesc
 	)
-	$csv = import-csv .\reprints.csv;
-	$prevRow = $csv[0]; 
 	foreach ($row in $csv[1..($csv.length-1)]) {
     	if (!$row.$subCatDesc) {
-			$row.$subCatDesc = $prevRow.$subCatDesc };
+			$row.$subCatDesc = $prevRow.$subCatDesc;
+		}
 			$prevRow = $row;
-			$csv | export-csv -Path .\reprints.csv -NoTypeInformation;
 	}
 }
 
-# Clean up rows with edits in them
-$csv = Import-CSV .\reprints.csv;
 foreach ($row in $csv) {
+	if (!$row.date) {
+		$row.date = (Get-Date -Format "MM/dd/yyyy");
+	#} else {
+		#csvFill -subCatDesc date
+	}
 	if ($row.error -match "\([A-z0-9 ]+\)") {
 		$row.error = ($row.error | Select-String "\(([A-z0-9 ]+)\)" | Select-object -expand matches | select-object -expand groups | select-object -expand value)[1];
 	}
 	if ($row.error -eq "edited") {
 		$row.error = "";
 	}
-	$csv | export-csv -Path .\reprints.csv -NoTypeInformation;
 }
 
-#wrap all the smaller functions into one command
-Function csvComplete {
-	$csv = import-csv -Path .\reprints.csv;
-	$csv | forEach-Object{
-		$_.date = (Get-Date -Format "MM/dd/yyyy");
-	#return $_;
-	}
-	$csv | export-csv -Path .\reprints.csv -NoTypeInformation;
-	csvFill -subCatDesc time
-	csvFill -subCatDesc name
-	csvFill -subCatDesc orderId
-}
+# Fill cells with previous row's data to complete every row
+csvFill -subCatDesc time
+csvFill -subCatDesc name
+csvFill -subCatDesc orderId
 
-csvComplete
+# Export the edited table as a csv
+$csv | export-csv -Path .\reprints.csv -NoTypeInformation;
 
-$csv = Import-CSV .\reprints.csv;
-
+# re-import the csv to print it into the console to verify data
+#$csv = Import-CSV .\reprints.csv;
 $csv | Format-Table;
